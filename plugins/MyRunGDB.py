1ed3851773390290554792e35d450aedc1cfbab8
nufeng1999 <18478162@21cn.com>
2021-12-20 22:51:12 +0800

    update plugins

--- plugins/MyRunGDB.py
+++ plugins/MyRunGDB.py
@@ -1,22 +1,30 @@
+##//%file:../../jupyter-MyPython-kernel/plugins/MyRunGDB.py
+##//%file:../../jupyter-MyDart-kernel/plugins/MyRunGDB.py
+##//%file:../../jupyter-MyKotlin-kernel/plugins/MyRunGDB.py
+##//%file:../../jupyter-MyGroovy-kernel/plugins/MyRunGDB.py
+##//%file:../../jupyter-MyJava-kernel/plugins/MyRunGDB.py
+##//%file:../../jupyter-MyGjs-kernel/plugins/MyRunGDB.py
+##//%file:../../jupyter-MyVala-kernel/plugins/MyRunGDB.py
+##//%file:../../jupyter-MyNodejs-kernel/plugins/MyRunGDB.py
 from typing import Dict, Tuple, Sequence,List
 from plugins.ISpecialID import IStag,IDtag,IBtag,ITag
 import os
-class MyNoruncode(IBtag):
+class MyRunGDB(IBtag):
     kobj=None
     def getName(self) -> str:
         # self.kobj._write_to_stdout("setKernelobj setKernelobj setKernelobj\n")
         
-        return 'MyNoruncode'
+        return 'MyShowpid'
     def getAuthor(self) -> str:
         return 'Author'
     def getIntroduction(self) -> str:
-        return 'MyNoruncode'
+        return 'MyShowpid'
     def getPriority(self)->int:
         return 0
     def getExcludeID(self)->List[str]:
         return []
     def getIDBptag(self) -> List[str]:
-        return ['noruncode']
+        return ['rungdb','gdb']
     def setKernelobj(self,obj):
         self.kobj=obj
         # self.kobj._write_to_stdout("setKernelobj setKernelobj setKernelobj\n")
@@ -25,8 +33,8 @@ class MyNoruncode(IBtag):
         return
     def on_IBpCodescanning(self,magics,line) -> str:
         # self.kobj._write_to_stdout(line+" on_ISpCodescanning\n")
-        self.kobj.addkey2dict(magics,'noruncode')
-        magics['noruncode'] = ['true']
+        self.kobj.addkey2dict(magics,'rungdb')
+        magics['rungdb'] = ['true']
         return ''
     ##在代码预处理前扫描代码时调用    
     def on_Codescanning(self,magics,code)->Tuple[bool,str]:
