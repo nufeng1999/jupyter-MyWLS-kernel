48570a8fa7eb0f515e3df0df5b07822ea8245277
nufeng1999 <18478162@21cn.com>
2022-01-11 17:17:45 +0800

    movmove pluginse plugins

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
-from setuptools import setup
-
-setup(name='jupyter_MyWLS_kernel',
+#!/usr/bin/env python
+# coding: utf-8
+with open("README.md", "r") as f:
+	long_description = f.read()
+import setuptools
+setuptools.setup(name='jupyter_MyWLS_kernel',
       version='0.0.1',
       description='Minimalistic vbscript kernel for Jupyter',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
       author='nufeng',
       author_email='18478162@qq.com',
       license='MIT',
-      classifiers=[
-          'License :: OSI Approved :: MIT License',
-      ],
       url='https://github.com/nufeng1999/jupyter-MyWLS-kernel/',
-      download_url='https://github.com/nufeng1999/jupyter-MyWLS-kernel/tarball/0.0.1',
-      packages=['jupyter_MyWLS_kernel'],
+      download_url='https://github.com/nufeng1999/jupyter-MyWLS-kernel/releases/tag/0.0.1',
+    packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
       scripts=['jupyter_MyWLS_kernel/install_MyWLS_kernel'],
       keywords=['jupyter', 'notebook', 'kernel', 'wls','Wolfram','WolframScript','Mathematica'],
       include_package_data=True
